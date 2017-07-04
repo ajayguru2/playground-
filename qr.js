4ec2d747ee41f655940fb7a8933356112f86d508
Ajay Guru <201651005@iiitvadodara.ac.in>
2017-07-04 13:25:56 -0700

    qr-code

---
+++ qr.js
@@ -0,0 +1,17 @@
+var yargs = require('yargs');
+var qr = require('qr-image');
+var fs = require('fs');
+
+
+
+var args = yargs.argv;
+
+console.log(args);
+
+if(args.code){
+  var qr_svg = qr.image(`${args.code}`, { type: 'svg' });
+  qr_svg.pipe(require('fs').createWriteStream(`${args.code}`));
+
+  var svg_string = qr.imageSync(`${args.code}`, { type: 'svg' });
+  console.log(`The given svg code is: ${svg_string}`);
+}
