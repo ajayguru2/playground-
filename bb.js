e2b67daac4e15dc5fc45017ff7a0c6b81edce28f
Ajay Guru <201651005@iiitvadodara.ac.in>
2017-07-08 19:32:36 +0530

    uplaod
    node school

---
+++ bb.js
@@ -0,0 +1,4 @@
+ const fs = require('fs');
+// // console.log("beep boop");
+// fs.createReadStream(`${process.argv[2]}`).pipe(process.stdout);
+process.stdin.pipe(process.stdout);
---
+++ foo.js
@@ -0,0 +1,9 @@
+function foo(){
+  var bar;
+  quux = 21;
+  return zip;
+  function zip(){
+    var quux = 97;
+    bar = true;
+  }
+}
